f832c5819d7ed68e8b228736a60aa26f4de90442
Ryan Zhu <rzhu@FinancialEngines.com>
2018-01-22 18:43:45 -0800

    add 2

---
+++ maxsumof3nonoverlappingsubarrays.py
@@ -0,0 +1,29 @@
+import sys
+class Solution:
+    def maxSumOfThreeSubarrays(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        if not nums or 3 * k > len(nums):
+            return []
+        n = len(nums)
+        sums = [0 for _ in range(n - k + 1)]
+        sums[0] = sum(nums[: k])
+        for i in range(1, len(sums)):
+            sums[i] = sums[i - 1] - nums[i - 1] + nums[i + k - 1]
+        dpfront = [[-sys.maxsize, 0] for _ in range(len(sums))]
+        dpback = [[-sys.maxsize, 0] for _ in range(len(sums))]
+        dpfront[0] = [sums[0], 0]
+        dpback[len(dpback) - 1] = [sums[len(sums) - 1], len(sums) - 1]
+        for i in range(1, len(dpfront)):
+            dpfront[i] = max(dpfront[i - 1], [sums[i], i], key = lambda item: item[0])
+        for i in range(len(dpback) - 2, -1, -1):
+            dpback[i] = max(dpback[i + 1], [sums[i], i], key = lambda item: item[0])
+        res, maxVal = [], -sys.maxsize
+        for i in range(k, len(sums) - k):
+            if sums[i] + dpfront[i - k][0] + dpback[i + k][0] > maxVal:
+                res = [dpfront[i - k][1], i, dpback[i + k][1]]
+                maxVal = sums[i] + dpfront[i - k][0] + dpback[i + k][0]
+        return res
---
+++ regularexpressionmatching.py
@@ -0,0 +1,30 @@
+class Solution:
+    def isMatch(self, s, p):
+        """
+        :type s: str
+        :type p: str
+        :rtype: bool
+        """
+        if s == None or p == None:
+            return False
+        m, n = len(s), len(p)
+        dp = [[False for _ in range(n + 1)]for _ in range(m + 1)]
+        dp[0][0] = True
+        for i in range(1, n + 1):
+            if p[i - 1] == '*':
+                dp[0][i] = dp[0][i - 2] if i > 1 else True #fill first row，
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                #处理 . 和 字符相等的情况
+                if p[j - 1] == '.' or p[j - 1] == s[i - 1]:
+                    dp[i][j] = dp[i - 1][j - 1]
+                # 处理 * 情况
+                elif p[j - 1] == '*' and j > 1:
+                    '''
+                    dp[i][j - 2] 匹配空字符串
+                    dp[i - 1][j] 匹配前 i - 1 个字符串和 前 j 个pattern，因为在*的情况下，前 i - 1 必须先匹配，才能保证之后的*匹配，
+                    因为*代表 preceding letters
+                    s[i - 1] == p[j - 2] or p[j - 2] == '.' 代表的就是 preceding letter
+                    '''
+                    dp[i][j] = dp[i][j - 2] or (dp[i - 1][j] and (s[i - 1] == p[j - 2] or p[j - 2] == '.'))
+        return dp[m][n]
